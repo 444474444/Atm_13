f3e92e719d6403e7b4ff349015a7b23a2403e8a2
gmansu1 <gmansu177>
2025-07-17 02:43:53 -0700

    🎨 추가 가능한 번호 추가
    입금(1),출금(2),영수증 보기(3) 선택가능한

--- atm.py
+++ atm.py
@@ -4,5 +4,16 @@ while True :
     num=  input("사용하실 기능의 번호를 선택해주세요 (1.입금, 2.출금, 3.영수증 보기, 4.종료):")
     if num == "4":
         break
+
+    if num == '1':
+        pass
+
+    if num =='2':
+        pass
+
+    if num =='3':
+        pass
+    
 print(f"시스템을 종료합니다. 현재 잔액은 {balance}")
 
+s
