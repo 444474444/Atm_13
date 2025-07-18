69629d7f43e2ea1ad06fd83cacc7517cc8d7bb9e
gmansu1 <gmansu177>
2025-07-17 19:22:54 -0700

    🎨 출금 기능 추가
    출금 금액 입력받아 잔액에서 차감하는 기능  구현 및 잔액보다  큰값 방지 기능 구현

--- atm.py
+++ atm.py
@@ -10,11 +10,13 @@ while True :
       balance+=deposit_amount 
       print(f"입금하신금액은 {deposit_amount}이고, 현재잔액은 {balance}입니다")
     if num =='2':
-        pass
+    
+      withdraw_amount =int(input('출금할 금액을 입력해주세요'))
+      withdraw_amount =min(balance,withdraw_amount)
+      balance-=withdraw_amount
+      print(f"출금 가능하신 금액은 {withdraw_amount}이고,현재잔액은{balance}입니다")
 
     if num =='3':
         pass
     
 print(f"시스템을 종료합니다. 현재 잔액은 {balance}")
-
-c
